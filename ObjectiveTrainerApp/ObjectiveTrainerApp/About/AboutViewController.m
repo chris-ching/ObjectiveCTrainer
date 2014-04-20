6c27940116cb9ffc7c36cafbdbdd66bd9c8e8b7f
Chris Ching <cwching@gmail.com>
2014-04-20 09:56:12 -0400

    About screen
    -Added elements to the about screen
    -Hooked up some of the button handlers in the about screen

--- ObjectiveTrainerApp/ObjectiveTrainerApp/About/AboutViewController.m
+++ ObjectiveTrainerApp/ObjectiveTrainerApp/About/AboutViewController.m
@@ -38,4 +38,32 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)feedbackButtonClicked:(id)sender
+{
+    MFMailComposeViewController *mailComposer = [[MFMailComposeViewController alloc] init];
+    mailComposer.mailComposeDelegate = self;
+    [mailComposer setSubject:@"Objective C Trainer Feedback"];
+    [mailComposer setToRecipients:@[@"codewithchris@gmail.com"]];
+    
+    [self presentViewController:mailComposer animated:YES completion:nil];
+}
+
+- (IBAction)rateButtonClicked:(id)sender
+{
+    
+}
+
+- (IBAction)moreTutorialsButtonClicked:(id)sender
+{
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://codewithchris.com"]];
+}
+
+#pragma mark Mail Compose Delegate Methods
+
+- (void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error
+{
+    // Dismiss the compose controller
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
 @end
